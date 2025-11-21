49adcd3a80193611b0d7048c9d5d99b200b7f08f
Muhammed Kole <muhammedkole18@gmail.com>
2025-11-21 23:50:03 +0300

    vercel.json kaldırıldı

---
+++ src/middleware.js
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+
+export function middleware(req) {
+  const url = req.nextUrl.clone();
+  const host = req.headers.get("host");
+
+  // Eğer subdomain "yonetim" ise
+  if (host?.startsWith("deneme.")) {
+    // Eğer zaten /admin değilse
+    if (!url.pathname.startsWith("/admin")) {
+      url.pathname = "/admin" + url.pathname;
+      return NextResponse.rewrite(url);
+    }
+  }
+
+  return NextResponse.next();
+}
+
+// Middleware hangi yollar için geçerli olacak
+export const config = {
+  matcher: ["/((?!_next/static|favicon.ico).*)"],
+};
